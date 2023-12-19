b56e5c8092e8782b6e53394afc4b85f306e3597b
r5 <ramzi.benyahia@gmail.com>
2023-12-19 16:19:30 +0000

    Displays a basic keyboard and changes color when key is pressed

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,67 @@ mod stats;
 use crate::msg::Msg;
 use crate::stats::Stats;
 
+const UNIT: f64 = 30.0;
+const QWERTY: &[(&str, f64)] = &[
+    ("`", 1.0),
+    ("1", 1.0),
+    ("2", 1.0),
+    ("3", 1.0),
+    ("4", 1.0),
+    ("5", 1.0),
+    ("6", 1.0),
+    ("7", 1.0),
+    ("8", 1.0),
+    ("9", 1.0),
+    ("0", 1.0),
+    ("-", 1.0),
+    ("=", 1.0),
+    ("del", 1.5), // line 1
+    ("tab", 1.5),
+    ("Q", 1.0),
+    ("W", 1.0),
+    ("E", 1.0),
+    ("R", 1.0),
+    ("T", 1.0),
+    ("Y", 1.0),
+    ("U", 1.0),
+    ("I", 1.0),
+    ("O", 1.0),
+    ("P", 1.0),
+    ("[", 1.0),
+    ("]", 1.0),
+    ("\\", 1.0), // line 2
+    ("caps", 2.0),
+    ("A", 1.0),
+    ("S", 1.0),
+    ("D", 1.0),
+    ("F", 1.0),
+    ("G", 1.0),
+    ("H", 1.0),
+    ("J", 1.0),
+    ("K", 1.0),
+    ("L", 1.0),
+    (";", 1.0),
+    ("'", 1.0),
+    ("enter", 2.0), // line 3
+    ("shift", 3.0),
+    ("Z", 1.0),
+    ("X", 1.0),
+    ("C", 1.0),
+    ("V", 1.0),
+    ("B", 1.0),
+    ("N", 1.0),
+    ("M", 1.0),
+    (",", 1.0),
+    (".", 1.0),
+    ("/", 1.0),
+    ("shift", 3.0), // line 4
+    ("space", 10.0),
+];
+const LAYOUT: &[usize] = &[14, 14, 13, 12, 1];
+const HSTART: f64 = 100.0;
+const VSTART: f64 = 100.0;
+
 #[relm4::component]
 impl SimpleComponent for Stats {
     type Init = Stats;
@@ -74,13 +135,7 @@ impl SimpleComponent for KeyboardState {
         _sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
         println!("Received an init");
-        let mut handler = DrawHandler::new();
-        let cx = handler.get_context();
-        let _ = cx.show_text("some text");
-        cx.set_source_rgb(0.0, 1.0, 0.0);
-        cx.move_to(0.0, 0.0);
-        cx.line_to(350.0, 350.0);
-        cx.stroke().expect("should draw something");
+        let handler = DrawHandler::new();
 
         let model = KeyboardState { practice, handler };
         let area = model.handler.drawing_area();
@@ -90,20 +145,47 @@ impl SimpleComponent for KeyboardState {
         ComponentParts { model, widgets }
     }
 
-    fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>) {
-        println!("Received an update");
+    fn update(&mut self, _message: Self::Input, _sender: ComponentSender<Self>) {
+        println!("received an update");
+        let key_pressed = match _message {
+            Msg::KeyPressed(k, _, _, _) => Some(k),
+        };
         let cx = self.handler.get_context();
         cx.select_font_face(
             "Arial Black",
             gtk::cairo::FontSlant::Normal,
             gtk::cairo::FontWeight::Bold,
         );
-        cx.set_source_rgb(0.0, 0.0, 0.0);
-        cx.set_font_size(12.0);
-        cx.move_to(10.0, 20.0);
 
-        cx.show_text("some text").expect("awful error");
-        cx.fill().expect("should fill something");
+        cx.set_source_rgb(0.0, 0.0, 0.0);
+        cx.set_font_size(18.0);
+        let mut x = VSTART;
+        let mut y = HSTART;
+        let mut iter = QWERTY.iter();
+        for (i, row) in LAYOUT.iter().enumerate() {
+            let mut row = *row;
+            while row > 0 {
+                cx.set_source_rgb(0.0, 0.0, 0.0);
+                row -= 1;
+                if let Some((cell, size)) = iter.next() {
+                    if let Some(key) = key_pressed {
+                        println!("key name is {:?}", key.name());
+                        if key
+                            .name()
+                            .is_some_and(|x| x.to_lowercase().as_str().eq_ignore_ascii_case(*cell))
+                        {
+                            cx.set_source_rgb(1.0, 0.0, 0.0);
+                        }
+                    }
+                    cx.move_to(x, y);
+                    cx.show_text(cell).expect("should display this char");
+                    x += UNIT * size;
+                    println!("{i}, {row}: {cell}: {size}");
+                }
+            }
+            x = VSTART;
+            y += UNIT;
+        }
     }
 }
 
